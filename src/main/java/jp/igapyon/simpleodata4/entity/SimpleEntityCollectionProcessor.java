336b79edb07248cb514ae3c3c5a3478ea4df1b35
Toshiki Iga <igapyon@gmail.com>
2021-02-27 19:27:13 +0900

    リファクタリング (#2)
    refactoring

--- src/main/java/jp/igapyon/simpleodata4/entity/SimpleEntityCollectionProcessor.java
+++ src/main/java/jp/igapyon/simpleodata4/entity/SimpleEntityCollectionProcessor.java
@@ -1,17 +1,11 @@
 package jp.igapyon.simpleodata4.entity;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 import org.apache.olingo.commons.api.data.ContextURL;
-import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.data.EntityCollection;
-import org.apache.olingo.commons.api.data.Property;
-import org.apache.olingo.commons.api.data.ValueType;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
-import org.apache.olingo.commons.api.ex.ODataRuntimeException;
 import org.apache.olingo.commons.api.format.ContentType;
 import org.apache.olingo.commons.api.http.HttpHeader;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
@@ -76,7 +70,8 @@ public class SimpleEntityCollectionProcessor implements EntityCollectionProcesso
         EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
 
         // 要素セットの指定をもとに要素コレクションを取得.
-        EntityCollection eCollection = getData(edmEntitySet);
+        // これがデータ本体に該当.
+        EntityCollection eCollection = SimpleEntityDataBuilder.buildData(edmEntitySet);
 
         // 指定のレスポンスフォーマットに合致する直列化を準備.
         ODataSerializer serializer = odata.createSerializer(responseFormat);
@@ -98,68 +93,4 @@ public class SimpleEntityCollectionProcessor implements EntityCollectionProcesso
         response.setStatusCode(HttpStatusCode.OK.getStatusCode());
         response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
     }
-
-    ///////////////////////////////////////////////////
-    // 実際に返却するエンティティデータの組み上げ.
-
-    /**
-     * 指定のEDM要素セットに対応する要素コレクションを取得.
-     * 
-     * @param edmEntitySet EDM要素セット.
-     * @return 要素コレクション.
-     */
-    private EntityCollection getData(EdmEntitySet edmEntitySet) {
-        EntityCollection eCollection = new EntityCollection();
-
-        if (SimpleEdmProvider.ES_PRODUCTS_NAME.equals(edmEntitySet.getName())) {
-            final List<Entity> eList = eCollection.getEntities();
-
-            // いくつかサンプルデータを作成.
-            final Entity e1 = new Entity() //
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[0], ValueType.PRIMITIVE, 1))
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[1], ValueType.PRIMITIVE, "MacBookPro16,2"))
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[2], ValueType.PRIMITIVE,
-                            "MacBook Pro (13-inch, 2020, Thunderbolt 3ポートx 4)"));
-            e1.setId(createId("Products", 1));
-            eList.add(e1);
-
-            final Entity e2 = new Entity() //
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[0], ValueType.PRIMITIVE, 2))
-                    .addProperty(
-                            new Property(null, SimpleEdmProvider.FIELDS[1], ValueType.PRIMITIVE, "MacBookPro E2015"))
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[2], ValueType.PRIMITIVE,
-                            "MacBook Pro (Retina, 13-inch, Early 2015)"));
-            e2.setId(createId("Products", 2));
-            eList.add(e2);
-
-            final Entity e3 = new Entity() //
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[0], ValueType.PRIMITIVE, 3))
-                    .addProperty(
-                            new Property(null, SimpleEdmProvider.FIELDS[1], ValueType.PRIMITIVE, "Surface Laptop 2"))
-                    .addProperty(new Property(null, SimpleEdmProvider.FIELDS[2], ValueType.PRIMITIVE,
-                            "Surface Laptop 2, 画面:13.5 インチ PixelSense ディスプレイ, インテル Core"));
-            e3.setId(createId("Products", 3));
-            eList.add(e3);
-        }
-
-        return eCollection;
-    }
-
-    /**
-     * 与えられた情報をもとにURIを作成.
-     * 
-     * @param entitySetName 要素セット名.
-     * @param id            ユニーク性を実現するId.
-     * @return 要素セット名およびユニーク性を実現するIdをもとにつくられた部分的なURI.
-     */
-    private URI createId(String entitySetName, Object id) {
-        try {
-            return new URI(entitySetName + "-" + String.valueOf(id));
-        } catch (URISyntaxException ex) {
-            throw new ODataRuntimeException("Fail to create ID EntitySet name: " + entitySetName, ex);
-        }
-    }
-
-    // 実際に返却するエンティティデータの組み上げ.
-    ///////////////////////////////////////////////////
 }
