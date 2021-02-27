0366ae4659ab281009cd74173d351dfa0d350991
Toshiki Iga <igapyon@gmail.com>
2021-02-27 15:06:06 +0900

    save

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Simple な OData v4 のサンプル
 
 ```sh
 http://localhost:8080/simple.svc/
+http://localhost:8080/simple.svc/Products
 ```
 
 
--- src/main/java/jp/igapyon/simpleodata4/DemoServlet.java
+++ src/main/java/jp/igapyon/simpleodata4/DemoServlet.java
@@ -16,6 +16,9 @@ import org.apache.olingo.server.api.ServiceMetadata;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * OData を Servlet として動作.
+ */
 @RestController
 public class DemoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -28,18 +31,15 @@ public class DemoServlet extends HttpServlet {
             OData odata = OData.newInstance();
             ServiceMetadata edm = odata.createServiceMetadata(new DemoEdmProvider(), new ArrayList<EdmxReference>());
             ODataHttpHandler handler = odata.createHandler(edm);
+            // EntityCollectionProcessor を登録.
             handler.register(new DemoEntityCollectionProcessor());
-
-            // handler.process(req, resp);
-            if (true) {
-                handler.process(new HttpServletRequestWrapper(req) {
-                    @Override
-                    public String getServletPath() {
-                        return "/simple.svc";
-                    }
-                }, resp);
-            }
-
+            // Spring と Servlet を調整.
+            handler.process(new HttpServletRequestWrapper(req) {
+                @Override
+                public String getServletPath() {
+                    return "/simple.svc";
+                }
+            }, resp);
         } catch (RuntimeException e) {
             System.err.println("Server Error occurred in ExampleServlet");
             throw new ServletException(e);
