de1830bde22bb5d1b5b179d4fc011943c3d5a840
Toshiki Iga <igapyon@gmail.com>
2021-02-28 10:37:30 +0900

    まがいにもCOUNTしている。

--- src/main/java/jp/igapyon/simpleodata4/entity/SimpleEntityCollectionProcessor.java
+++ src/main/java/jp/igapyon/simpleodata4/entity/SimpleEntityCollectionProcessor.java
@@ -22,6 +22,8 @@ import org.apache.olingo.server.api.serializer.SerializerResult;
 import org.apache.olingo.server.api.uri.UriInfo;
 import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
+import org.apache.olingo.server.api.uri.queryoption.CountOption;
+import org.apache.olingo.server.api.uri.queryoption.SystemQueryOptionKind;
 
 /**
  * OData 要素コレクションを処理するクラス.
@@ -72,7 +74,7 @@ public class SimpleEntityCollectionProcessor implements EntityCollectionProcesso
 
         // 要素セットの指定をもとに要素コレクションを取得.
         // これがデータ本体に該当.
-        EntityCollection eCollection = SimpleEntityDataBuilder.buildData(edmEntitySet, uriInfo);
+        final EntityCollection eCollection = SimpleEntityDataBuilder.buildData(edmEntitySet, uriInfo);
 
         // 指定のレスポンスフォーマットに合致する直列化を準備.
         ODataSerializer serializer = odata.createSerializer(responseFormat);
@@ -83,9 +85,34 @@ public class SimpleEntityCollectionProcessor implements EntityCollectionProcesso
 
         // 要素のIdを作成.
         final String id = request.getRawBaseUri() + "/" + edmEntitySet.getName();
+        final CountOption copt = new CountOption() {
+            @Override
+            public SystemQueryOptionKind getKind() {
+                return SystemQueryOptionKind.COUNT;
+            }
+
+            @Override
+            public String getName() {
+                // TODO Auto-generated method stub
+                return "COUNT";
+            }
+
+            @Override
+            public String getText() {
+                return "" + eCollection.getEntities().size();
+            }
+
+            @Override
+            public boolean getValue() {
+                // TODO Auto-generated method stub
+                return true;
+            }
+        };
+        // TODO 考慮不足。件数ではなくて別途COUNTすべき。
+        eCollection.setCount(eCollection.getEntities().size());
         // 直列化の処理.
         EntityCollectionSerializerOptions opts = EntityCollectionSerializerOptions.with() //
-                .id(id).contextURL(conUrl).build();
+                .id(id).count(copt).contextURL(conUrl).build();
         SerializerResult serResult = serializer.entityCollection( //
                 serviceMetadata, edmEntityType, eCollection, opts);
 
