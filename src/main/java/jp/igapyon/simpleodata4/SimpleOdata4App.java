be740f5e624fb5298c112cd0c20c377472884c5e
Toshiki Iga <igapyon@gmail.com>
2021-03-07 11:39:33 +0900

    4タブでフォーマット。

--- src/main/java/jp/igapyon/simpleodata4/SimpleOdata4App.java
+++ src/main/java/jp/igapyon/simpleodata4/SimpleOdata4App.java
@@ -8,12 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SimpleOdata4App {
-	/**
-	 * OData サーバ (Spring Boot ベース) を起動するエントリポイント.
-	 * 
-	 * @param args コマンドライン引数.
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleOdata4App.class, args);
-	}
+    /**
+     * OData サーバ (Spring Boot ベース) を起動するエントリポイント.
+     * 
+     * @param args コマンドライン引数.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOdata4App.class, args);
+    }
 }
