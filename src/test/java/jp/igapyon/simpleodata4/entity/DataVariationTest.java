a13d46df4151dbefc9ddc2478b7b2f86b9db804a
Toshiki Iga <igapyon@gmail.com>
2021-03-14 09:37:50 +0900

    save

--- src/test/java/jp/igapyon/simpleodata4/entity/DataVariationTest.java
+++ src/test/java/jp/igapyon/simpleodata4/entity/DataVariationTest.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.olingo.commons.api.http.HttpMethod;
@@ -20,6 +19,24 @@ import org.junit.jupiter.api.Test;
 class DataVariationTest {
     @Test
     void test01() throws Exception {
+        final ODataHttpHandler handler = getHandler();
+        final ODataRequest req = new ODataRequest();
+        req.setMethod(HttpMethod.GET);
+        req.setRawBaseUri("http://localhost:8080/simple.svc");
+        req.setRawODataPath("/MyProducts");
+        req.setRawQueryPath("$top=6&$search=macbook&$count=true&$select=ID");
+        req.setRawRequestUri(req.getRawBaseUri() + req.getRawODataPath() + "?" + req.getRawQueryPath());
+
+        final ODataResponse resp = handler.process(req);
+        assertEquals(200, resp.getStatusCode());
+        assertEquals("{\"@odata.context\":\"$metadata#MyProducts\",\"value\":[{\"ID\":1},{\"ID\":2}]}",
+                stream2String(resp.getContent()));
+    }
+
+    ////////////////////////////////////////////////////////
+    // 以降は共通コード.
+
+    private ODataHttpHandler getHandler() throws Exception {
         final OData odata = OData.newInstance();
 
         // EdmProvider を登録.
@@ -28,19 +45,16 @@ class DataVariationTest {
 
         // EntityCollectionProcessor を登録.
         handler.register(new SimpleEntityCollectionProcessor());
-
-        final ODataRequest request = new ODataRequest();
-        request.setMethod(HttpMethod.GET);
-        request.setRawBaseUri("http://localhost:8080/simple.svc");
-        request.setRawODataPath("/MyProducts");
-        request.setRawQueryPath("$top=6&$search=macbook&$count=true&$select=ID");
-        request.setRawRequestUri(request.getRawBaseUri() + request.getRawODataPath() + "?" + request.getRawQueryPath());
-        ODataResponse response = handler.process(request);
-        assertEquals(200, response.getStatusCode());
-        assertEquals("{\"@odata.context\":\"$metadata#MyProducts\",\"value\":[{\"ID\":1},{\"ID\":2}]}",
-                stream2String(response.getContent()));
+        return handler;
     }
 
+    /**
+     * InputStream を String に変換.
+     * 
+     * @param inStream 入力ストリーム.
+     * @return 文字列.
+     * @throws IOException 入出力例外が発生した場合.
+     */
     private static String stream2String(InputStream inStream) throws IOException {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inStream, "UTF-8"))) {
