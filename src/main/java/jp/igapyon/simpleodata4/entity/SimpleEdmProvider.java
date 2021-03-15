805c0c6f7b1c0687bc43f9946905d3a955800811
Toshiki Iga <igapyon@gmail.com>
2021-03-15 21:17:52 +0900

    itworks.

--- src/main/java/jp/igapyon/simpleodata4/entity/SimpleEdmProvider.java
+++ src/main/java/jp/igapyon/simpleodata4/entity/SimpleEdmProvider.java
@@ -37,7 +37,7 @@ public class SimpleEdmProvider extends CsdlAbstractEdmProvider {
     @Override
     public CsdlEntityType getEntityType(FullQualifiedName entityTypeName) {
         if (entityTypeName
-                .equals(localContainerInfo.getLocalEntityInfoByEntityNameFQN(entityTypeName).getEntityNameFQN())) {
+                .equals(localContainerInfo.getLocalEntityInfoByEntityNameFQN(entityTypeName).getInternalEntityNameFQN())) {
             // 処理対象の型名です。
             return localContainerInfo.getLocalEntityInfoByEntityNameFQN(entityTypeName).getEdmBuilder().getEntityType();
         }
@@ -66,7 +66,7 @@ public class SimpleEdmProvider extends CsdlAbstractEdmProvider {
         // CSDL要素セットとして情報を組み上げ.
         OiyokanCsdlEntitySet entitySet = new OiyokanCsdlEntitySet();
         entitySet.setName(entitySetName);
-        entitySet.setType(localContainerInfo.getLocalEntityInfoByEntitySetName(entitySetName).getEntityNameFQN());
+        entitySet.setType(localContainerInfo.getLocalEntityInfoByEntitySetName(entitySetName).getInternalEntityNameFQN());
 
         return entitySet;
 
@@ -85,7 +85,7 @@ public class SimpleEdmProvider extends CsdlAbstractEdmProvider {
         List<CsdlEntitySet> entitySets = new ArrayList<>();
         for (SimpleEntityInfo localEntryInfo : localContainerInfo.getLocalEntityInfoList()) {
             // TODO 増殖か?
-            entitySets.add(getEntitySet(localContainerInfo.getContainerFQN(), localEntryInfo.getEntitySetName()));
+            entitySets.add(getEntitySet(localContainerInfo.getContainerFQN(), localEntryInfo.getInternalEntitySetName()));
         }
 
         // 要素コンテナを作成.
@@ -111,7 +111,7 @@ public class SimpleEdmProvider extends CsdlAbstractEdmProvider {
         List<CsdlEntityType> entityTypes = new ArrayList<>();
         for (SimpleEntityInfo localEntryInfo : localContainerInfo.getLocalEntityInfoList()) {
             // TODO 増殖.
-            entityTypes.add(getEntityType(localEntryInfo.getEntityNameFQN()));
+            entityTypes.add(getEntityType(localEntryInfo.getInternalEntityNameFQN()));
         }
 
         schema.setEntityTypes(entityTypes);
